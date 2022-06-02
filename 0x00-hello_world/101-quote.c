2d3a1ab8efb807381e98d26777e0dbac954b8c9e
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-03 00:41:31 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Description: print a quote using a function
- *
- * Return: return 1
- */
+* main - Entry point
+*
+* Description: print a quote using a function
+*
+* Return: return 1
+*/
 int main(void)
 {
 	char quo[] = "and that place of are is useful\" - Dore Korpar, 2015-10-19\n";
-	write(1, quo, 59);
 	
+	write(1, quo, 59);
 	return (1);
 }
