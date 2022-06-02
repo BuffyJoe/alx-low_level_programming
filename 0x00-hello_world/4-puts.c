5bdac48f15cfe64d7c7d82d0292af3b411b9d443
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-02 21:10:20 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
-	 * this is a function that prints a single line of code
-	 *
-	 * it is going to always retun 0
+	* this is a function that prints a single line of code
+	*
+	* it is going to always retun 0
 */
-
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
