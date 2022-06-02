69188128ce2b33106ece82bd6d91d88cb17c8e04
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-02 23:41:55 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always ) (seccuess)
-*/
-
+ * main - Entry point
+ *
+ * prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char acharacter;
-	int aninteger;
-	long along;
-	long long alonglong;
-	float afloat;
+printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-
-	printf("Size of a int: %lu byte(s)\n", sizeof(aninteger));
-
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-
-	return  (0);
+return (0);
 }
