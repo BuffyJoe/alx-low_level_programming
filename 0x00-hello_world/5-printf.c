746b88fb3bb1003e1de02006ee9c9f04dcbc78a0
Okafor Onyekachukwu <okaforbestkid@gmail.com>
2022-06-02 13:36:13 -0700

    new

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *
+ * 	 main - entry point
+ *
+ * 	 this is a function that prints a single line of code
+ *
+ * 	
+ *
+ *	 Return: it is going to always retun 0
+ *
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,");
+
+		return (0);
+		/* return - is 0 */
+
+}
