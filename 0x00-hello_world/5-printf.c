ac4fe7856a5f2c6559317c3e49095f050a526740
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-02 21:37:46 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
-
 /**
- *
- * 	 main - entry point
- *
- * 	 this is a function that prints a single line of code
- *
- * 	
- *
- *	 Return: it is going to always retun 0
- *
- */
-
+*
+* 	 main - entry point
+*
+* 	 this is a function that prints a single line of code
+*
+* 	
+*
+*	 Return: it is going to always retun 0
+*
+*/
 int main(void)
-
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,");
-
 		return (0);
-		/* return - is 0 */
-
 }
