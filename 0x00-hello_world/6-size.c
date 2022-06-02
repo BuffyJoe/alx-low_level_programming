d56c1238314c25dd1e3f8714314c4dfe61a3f871
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-02 23:00:51 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -22,7 +22,7 @@ int main(void)
 
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat)'\n');
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat)');
 
 	return  (0);
 }
