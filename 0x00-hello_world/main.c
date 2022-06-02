386f79f6ff529c5ae9e7164233a811a0393bd37e
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-02 15:21:03 +0100

    Update main.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,1 +1,12 @@
-this is a line i added into this
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Holberton School");
+	return (0);
+}
