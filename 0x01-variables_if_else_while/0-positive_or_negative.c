ff34e7816352100543f97af60718ae3c339026cc
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-03 20:40:27 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,17 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: program that assign a random number n each time it is executed
- *
- * Return: return one of the 3 options
- */
+* main - Entry point
+*
+* Description: program that assign a random number n each time it is executed
+*
+* Return: return one of the 3 options
+*/
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
