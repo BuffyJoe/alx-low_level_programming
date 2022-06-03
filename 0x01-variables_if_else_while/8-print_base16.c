f8362717b36ddeebabbea44dc0383ff53f9a14fe
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-03 22:12:39 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,8 +8,12 @@
 int main(void)
 {
 	char c;
-
-	for (c = 'a'; c <= 'z'; ++c)
+	int a;
+	for (a = 0; a < 10; ++a)
+	{
+		putchar((a) + '0');
+	}
+	for (c = 'a'; c <= 'f'; ++c)
 	{
 		putchar(c);
 	}
