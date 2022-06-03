29deebf9cdb9e7c5c3e9a77b8232b00f5353c5ec
Okafor Onyekachukwu <okaforbestkid@gmail.com>
2022-06-03 13:39:03 -0700

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all the alphabets in lower case
+ *
+ * Return: return all the alphabets
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c != 'e' || c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
