562ca7a206ec9b7267f22baeab58891a756b9d58
Okafor Onyekachukwu <okaforbestkid@gmail.com>
2022-06-07 11:17:34 -0700

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * main - entry point
+ *
+ * Description: program that prints -purchar
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	putchar("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my read me for 0x02-functions_nested_loops
