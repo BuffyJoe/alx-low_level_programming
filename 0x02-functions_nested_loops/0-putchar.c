350a73856bd54c5904852ed7b14f9039f7c3d22f
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-08 05:05:32 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,24 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * main - entry point
+ * main - Entry point
  *
- * Description: prints _putchar
+ * code: prints out word _putchar, followed by new line.
  *
- * Return: 0 = success
+ * Return: retrun 0 = success
  */
 int main(void)
 {
 	char word[8] = "_putchar";
 	int i;
+
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(word[i]);
+
+	_putchar(word[i]);
+
 	}
 	_putchar('\n');
+
 	return (0);
 }
