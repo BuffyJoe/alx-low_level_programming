9068273dfab4b8a3b3cd5c77b96aeb1995c0333d
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-09 19:29:27 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,15 @@
 void print_most_numbers(void)
 {
 	int i;
+	int b;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if ((i == 2) || (i == 4))
-			continue;
-		else
-			_putchar(i + '0');
+		while (b = 0; b <= 14; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
