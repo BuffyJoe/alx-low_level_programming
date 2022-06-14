508af8d48d32f1d080c1479ea8f59989dbc3aa2c
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-14 04:52:49 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,24 @@
+)  257 Bytes
 #include "main.h"
-
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
- */
+* *_strcpy - function
+* @dest: parameter
+* @src: parameter
+*
+* Return: dest
+*/
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	for (; src[i] != '\0'; i++)
+	i = 0;
+
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
