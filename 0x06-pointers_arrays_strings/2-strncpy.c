dba7640b6218c22eab61a23cb662223a1020beb2
Monkey D Luffy <81034050+BuffyJoe@users.noreply.github.com>
2022-06-15 20:17:33 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,22 @@
 #include "main.h"
-
 /**
- * _strncpy - Entry point
- * @dest: copy to
- * @src: copy from
- * @n: input number of char
- * Return: Always 0 (Success)
+ * *_strncpy - function that copies a string
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ *
+ * Return: pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
-
+	for (i = 0; i < n && src[i] != 0; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = 0;
+		i++;
+	}
 	return (dest);
 }
